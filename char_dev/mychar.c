fb4a96fecd5c95f513ff40f2f4fe11d356d4dc12
ray-jk-hong <ray.jk.hong@gmail.com>
2024-08-22 20:38:57 +0800

    hfa

--- char_dev/mychar.c
+++ char_dev/mychar.c
@@ -4,6 +4,7 @@
 #include <linux/ioctl.h>
 #include <linux/miscdevice.h>
 
+#define MY_CHAR_NAME "my_dev"
 static struct miscdevice misc_dev;
 
 static int my_open(struct inode *inode, struct file *filp)
@@ -18,20 +19,40 @@ static int my_release(struct inode *inode, struct file *filp)
     return 0;
 }
 
+static ssize_t my_write(struct file *file, const char __user *buf,
+    size_t lbuf, loff_t *ppos)
+{
+    return 0;
+}
+
+static loff_t my_lseek(struct file *file, loff_t offset, int orig) 
+{
+    return 0;
+}
+
+static ssize_t my_read(struct file *file, char __user *buf,
+    size_t lbuf, loff_t *ppos)
+{
+    return 0;
+}
+
 static const struct file_operations my_fops = {
     .owner = THIS_MODULE,
     .open = my_open,
-    .release = my_release
+    .release = my_release,
+    .read = my_read,
+    .write = my_write,
+    .llseek = my_seek
 };
 
 static int __init my_inint(void)
 {
     int ret;
 
-    misc_dev.name = "my_dev";
+    misc_dev.name = "MY_CHAR_NAME";
     misc_dev.minor = MISC_DYNAMIC_MINOR;
     misc_dev.fops = &my_fops;
-    misc_dev.mode = 0600;
+    misc_dev.mode = 0660;
     ret = misc_register(&misc_dev);
     if (ret != 0) {
         pr_err("Misc register fail. (ret=%d)\n", ret);
