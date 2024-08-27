f2b5a6c116fbd4d224128ca48f77e8a1166692ae
ray-jk-hong <ray.jk.hong@gmail.com>
2024-08-27 22:56:36 +0800

    afahaf

---
+++ trace/trace_marker/trace_marker.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#define TRACE_PATH "/sys/kernel/debug/tracing/trace_marker"
+static int trace_fd = -1;
+
+static void trace_write(const char *fmt, ...)
+{
+    va_list ap;
+    char buf[256];
+    int n;
+
+    if (trace_fd < 0) {
+        printf("trace open fail\n");
+        return;
+    }
+    va_start(ap, fmt);
+	n = vsnprintf(buf, 256, fmt, ap);
+	va_end(ap);
+
+	write(trace_fd, buf, n);    
+}
+
+static int trace_open(void)
+{
+    trace_fd = open(TRACE_PATH, O_WRONLY);
+    if (trace_fd < 0) {
+        printf("trace open fail\n");
+        return -1;
+    }
+    return 0;
+}
+
+static void trace_close(void)
+{
+    if (trace_fd >= 0) {
+        close(trace_fd);
+        trace_fd = -1;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int ret;
+
+    ret = trace_open();
+    if (ret < 0) {
+        printf("trace open fail\n");
+        return ret;
+    }
+    trace_write("xxxxxxxxxxxxxyyyyyyyyyyyy\n");
+    trace_close();
+
+    return 0;
+}
