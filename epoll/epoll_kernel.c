8dc7ae46e45ed1e9d656980b5888b5fa7f0cd7cd
ray-jk-hong <ray.jk.hong@gmail.com>
2024-10-01 16:03:03 +0800

    hafa

--- epoll/epoll_kernel.c
+++ epoll/epoll_kernel.c
@@ -1,5 +1,29 @@
 #include <linux/types.h>
 #include <linux/module.h>
+#include <linux/wait.h>
+ #include <linux/poll.h>
+ 
+static unsigned int epoll_poll(struct file *file, poll_table *wait)
+{
+    return 0;
+}
+
+static int epoll_open(struct inode *inode, struct file *file)
+{
+    return 0;
+}
+
+static int epoll_release(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+static const struct file_operations epoll_fops = {
+    .owner = THIS_MODULE,
+    .open = epoll_open,
+    .release = epoll_release,
+    .poll = epoll_poll
+};
 
 static int __init epoll_init(void)
 {
